4e9a2dfaa44b9c64378ac1218f13bf70e8949ef7
Iwan978 <155195552+Iwan978@users.noreply.github.com>
2024-07-08 23:01:41 +0700

    first commit

---
+++ settings.js
@@ -0,0 +1,39 @@
+const fs = require('fs')
+
+global.creator = 'Ryuuken'// yourname
+global.MONGO_DB_URI = "mongodb+srv://Ryuuken97:@Ryuuking97@cluster0.lopfmdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0" //database mongodb 
+global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
+global.your_email = "ryuukencifer@gmail.com" //email
+global.email_password = "bgsl slno yqxm pzfq" //application password email
+global.limitCount = 100
+global.YUOR_PORT = 8000
+global.loghandler = {
+	noapikey:{
+		status: 403,
+        message: 'Input parameter apikey',
+        creator: `${creator}`,
+        result: "error"
+    },
+    error: {
+        status: 503,
+        message: 'Service Unavaible, Sedang dalam perbaikan',
+        creator: `${creator}`
+    },
+    apikey: {
+    	status: 403,
+    	message: 'Forbiden, Invalid apikey',
+    	creator: `${creator}`
+    },
+    noturl: {
+    	status: 403,
+    	message: 'Forbiden, Invlid url, masukkan parameter url',
+    	creator: `${creator}`,
+    }
+}
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(`Update'${__filename}'`)
+	delete require.cache[file]
+	require(file)
+})
