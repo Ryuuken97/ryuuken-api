7ff5de19f85e9f388b585b40f77129882a2c5c5b
Ryuuken <133407503+Ryuuken97@users.noreply.github.com>
2024-07-09 03:58:24 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,2 +1,1 @@
 //dbd
-{"_id":{"$oid":"668c51adfae1e0f8b28a9647"},"premium":"true","username":"Ryuuken97","email":"akunrd2@gmail.com","password":"OZO0wVCx3fFRMUVeT3KIe/un1VR092yvuqRigv0tq3E=","apikey":"Ryuuken97","defaultKey":"HvKRUsaQLb","limit":{"$numberInt":"10000"}}
